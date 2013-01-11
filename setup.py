f1228d8d84f601adf374eedd2d958c70bd136030
Paul McLanahan <paul@mclanahan.net>
2013-01-10 22:22:04 -0500

    Bug 829202: Update setup.py in prep for push to PyPI.

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
 from setuptools import setup, find_packages
 
+import basket as distmeta
+
 
 setup(
-    name='basket',
-    version='0.3.0',
-    description="A Python client for Mozilla's basket service",
+    name='basket-client',
+    version=distmeta.__version__,
+    description=distmeta.__doc__,
     long_description=open('README.rst').read(),
-    author='Michael Kelly',
-    author_email='mkelly@mozilla.com',
-    license='BSD',
+    author=distmeta.__author__,
+    author_email=distmeta.__contact__,
+    license=distmeta.__license__,
     packages=find_packages(exclude=['ez_setup']),
     install_requires=['requests'],
-    url='https://github.com/mozilla/basket-client',
+    url=distmeta.__homepage__,
     include_package_data=True,
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         'Intended Audience :: Developers',
         'Natural Language :: English',
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Communications",
         'Topic :: Software Development :: Libraries',
-        ],
+    ],
     keywords=['mozilla', 'basket']
 )
