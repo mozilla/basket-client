6d9aff6c8902017dafcf250ccbc9fba002e44141
Paul McLanahan <pmac@mozilla.com>
2013-06-10 10:56:07 -0400

    Fix subscribe method to accept an iterable of newsletters.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='basket-client',
-    version='0.3.3',
+    version='0.3.4',
     description="A Python client for Mozilla's basket service.",
     long_description=open('README.rst').read(),
     author='Michael Kelly and contributors',
