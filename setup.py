1c8e33f993214b554c8a95520c6ef1c721d3f38e
Rob Hudson <robhudson@mozilla.com>
2023-05-26 11:47:25 -0700

    Move tox config to toml file

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
